0cf9b8ca7b3eba17d7a2081c9d41a94d1c351476
satchmo2002 <satchwill.aaron@gmail.com>
2017-06-03 03:27:27 +0000

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   
   resources :articles
   
+  get 'signup', to: 'users#new'
+  resources :users, execpt: [:new]
+  
   #get 'articles/new', to: 'articles#new'
 
   # Example of regular route:
